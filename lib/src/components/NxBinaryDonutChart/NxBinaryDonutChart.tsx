8bd370ad4a9019624cf4911bb9549dbcc5d66565
Ross Pokorny <rpokorny@sonatype.com>
2020-10-16 09:34:03 -0400

    Remove unneeded parens - RSC-335

--- lib/src/components/NxBinaryDonutChart/NxBinaryDonutChart.tsx
+++ lib/src/components/NxBinaryDonutChart/NxBinaryDonutChart.tsx
@@ -20,7 +20,7 @@ const NxBinaryDonutChart = forwardRef<SVGSVGElement, Props>(
 
       // add 0.5π because we want the angle from the top of the circle, not the right
       const arcEndAngle = (props.percent / 100) * 2 * Math.PI + 0.5 * Math.PI;
-      const arcEndX = (Math.cos(arcEndAngle) * r);
+      const arcEndX = Math.cos(arcEndAngle) * r;
       const arcEndY = Math.sin(arcEndAngle) * r;
 
       // note: this doesn't behave as expected when percent = 0 or 100 hence the special cases in the rendering below
